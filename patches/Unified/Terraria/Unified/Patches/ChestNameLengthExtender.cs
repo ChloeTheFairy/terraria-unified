001839521dab5338d4a002e8a120d4ea3a8d403b
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-02-04 19:00:25 -0600

    feat(#2): raise chest name limit

---
+++ patches/Unified/Terraria/Unified/Patches/ChestNameLengthExtender.cs
@@ -0,0 +1,15 @@
+﻿namespace Terraria.Unified.Patches;
+
+internal static class ChestNameLengthExtender
+{
+	public static int VanillaMaxChestNameLength => Chest.MaxNameLength;
+
+	// Can be an arbitrary number...
+	public static int ModdedMaxChestNameLength => 63;
+
+	// TODO: Conditionally set this based on possible preferences.  i.e. would
+	// it be unwise to attempt to bypass this on a TShock server?
+	public static bool BypassServerNameLengthChecks => true;
+
+	public static int PreferredMaxChestNameLength => BypassServerNameLengthChecks ? ModdedMaxChestNameLength : VanillaMaxChestNameLength;
+}
