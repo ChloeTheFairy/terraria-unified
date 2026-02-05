0056c3d6fd63ce01de891819807359dc1ac44e53
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-02-04 18:03:57 -0600

    feat: remove tile safety checks in non-debug builds

--- patches/Unified/Terraria/Tilemap.cs
+++ patches/Unified/Terraria/Tilemap.cs
@@ -1,7 +1,6 @@
-﻿using System;
+﻿using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using Terraria.DataStructures;
-using Terraria.IO;
 
 namespace Terraria;
 
@@ -10,37 +9,10 @@ public readonly struct Tilemap
 	public readonly ushort Width;
 	public readonly ushort Height;
 
-	// Unsafe getters ignore bounds checks for scenarios where it's known to be
-	// safe.
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public Tile UnsafeGet(int x, int y)
-	{
-		return new Tile((uint)(y + (x * Height)));
-	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public Tile UnsafeGet(Point p)
-	{
-		return UnsafeGet(p.X, p.Y);
-	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public Tile UnsafeGet(Point16 p)
-	{
-		return UnsafeGet(p.X, p.Y);
-	}
-
 	public Tile this[int x, int y] {
-		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		get {
-			if ((uint)x >= Width || (uint)y >= Height) {
-				throw new IndexOutOfRangeException();
-
-				// The informative version is unfortunately terrible for performance (makes worldgen take 2.15x longer)
-				// throw new IndexOutOfRangeException($"({x}, {y}). Map size ({Width}, {Height})");
-			}
-
+			Debug.Assert(x >= 0 && x < Width && y >= 0 && y < Height);
 			return new Tile((uint)(y + (x * Height)));
 		}
 		internal set {
@@ -52,7 +24,7 @@ public readonly struct Tilemap
 
 	public Tile this[Point pos] => this[pos.X, pos.Y];
 
-	public Tile this[DataStructures.Point16 pos] => this[pos.X, pos.Y];
+	public Tile this[Point16 pos] => this[pos.X, pos.Y];
 
 	internal Tilemap(ushort width, ushort height)
 	{
