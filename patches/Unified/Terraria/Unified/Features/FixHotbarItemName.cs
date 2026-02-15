2771a8e6acd1de6c18dd8e9e75881069432da17f
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-02-14 20:24:18 -0600

    feat(#22): better item name rendering in the hotbar

---
+++ patches/Unified/Terraria/Unified/Features/FixHotbarItemName.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+using Terraria.GameContent.UI;
+using Terraria.ID;
+using Terraria.UI.Chat;
+
+namespace Terraria.Unified.Features;
+
+internal static class FixHotbarItemName
+{
+	public static bool Enabled { get; set; } = true;
+
+	public static void DrawItemName(SpriteBatch sb, Vector2 pos, string itemName, Item item)
+	{
+		var color = ItemRarity.GetColor(item.rare);
+		if (item.expert || item.rare == ItemRarityID.Expert) {
+			color = new Microsoft.Xna.Framework.Color(Main.DiscoR, Main.DiscoG, Main.DiscoB);
+		}
+		else if (item.rare == ItemRarityID.Master) {
+			color = new Microsoft.Xna.Framework.Color(255, (byte)(Main.masterColor * 200f), 0);
+		}
+
+		var alpha = Main.mouseTextColor / 255f;
+		ChatManager.DrawColorCodedStringWithShadow(sb, FontAssets.MouseText.Value, itemName, pos, color * alpha, 0f, Vector2.Zero, Vector2.One);
+	}
+}
