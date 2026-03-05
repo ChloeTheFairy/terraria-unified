e820a6f4426682b2015f82510fa0ab4fa72867e4
lost-werewolf <sebmartinpersonal@gmail.com>
2026-03-05 17:54:19 -0500

    feat(#36): preliminary work for xl worlds, support tilemap and worldmap resizing (#29)
    * Initial XL World Support
    
    * Implement tML fullscreen map dynamic draw
    - Only used for non-vanilla world size
    
    * feat: normalize method bodies
    
    * feat: more patch cleanups
    
    * feat: hopefully final patch cleanup changes
    
    * fix: correct jump
    
    ---------
    
    Co-authored-by: Stephen Thiebaud <xxlennygamerxx@gmail.com>

---
+++ patches/Unified/Terraria/GameContent/LeashedEntity.Unified.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terraria.GameContent;
+
+public partial class LeashedEntity
+{
+	public static void Resize()
+	{
+		BySection = new SectionEntityList[Main.maxTilesX / 200 + 1, Main.maxTilesY / 150 + 1];
+	}
+}
---
+++ patches/Unified/Terraria/MapRenderer.Unified.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Terraria;
+
+public partial class MapRenderer
+{
+	public static void Resize()
+	{
+		for (int x = 0; x < numTargetsX; x++) {
+			for (int y = 0; y < numTargetsY; y++) {
+				if (mapTarget[x, y] != null && !mapTarget[x, y].IsDisposed)
+					mapTarget[x, y].Dispose();
+			}
+		}
+
+		numTargetsX = Main.maxTilesX / textureMaxWidth + 1;
+		numTargetsY = Main.maxTilesY / textureMaxHeight + 1;
+		mapTarget = new RenderTarget2D[numTargetsX, numTargetsY];
+		initMap = new bool[numTargetsX, numTargetsY];
+		mapWasContentLost = new bool[numTargetsX, numTargetsY];
+		changeQueues = new List<Point16>[numTargetsX, numTargetsY];
+		for (int x = 0; x < numTargetsX; x++) {
+			for (int y = 0; y < numTargetsY; y++) {
+				changeQueues[x, y] = new List<Point16>(capacity: ChangeRefreshThreshold);
+			}
+		}
+	}
+}
