8c4a97409ba714ef274eaa1076db7036395210b2
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-08-20 07:20:36 -0500

    fix: quick patch to fix audio

--- patches/Unified/Terraria/Main.Unified.cs
+++ patches/Unified/Terraria/Main.Unified.cs
@@ -13,7 +13,7 @@ namespace Terraria;
 
 partial class Main
 {
-	internal static string UnifiedVersion => "0.3.1";
+	internal static string UnifiedVersion => "0.3.2";
 
 	internal static bool UnifiedBranding => true;
 
